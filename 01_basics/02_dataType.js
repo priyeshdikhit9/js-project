c5407ba5b73cbd4fa6dce7991748d6b45ac7685d
priyeshdikhit9 <139910173+priyeshdikhit9@users.noreply.github.com>
2024-05-05 04:50:17 +0000

    data types

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+let name="Priyesh"
+let age=26
+let isLoggedIn= false
+
+//number => 2 to power 52
+// bigint
+//string  => ""
+//boolean= true/false
+//null => standalone value
+//undefined 
+// symbol => unique
+
+//object
+
+console.log(typeof"Priyesh");
